bfe8ffddab62dd42578bb877bc5f0bda82edf255
Daniel Stiner <danstiner@gmail.com>
2017-07-30 00:26:07 -0700

    Translate RDESC

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,151 @@
+/*
+ * UHID Example
+ *
+ * Copyright (c) 2012-2013 David Herrmann <dh.herrmann@gmail.com>
+ *
+ * Converted from c to rust by Daniel Stiner <daniel.stiner@gmail.com>
+ *
+ * The code may be used by anyone for any purpose,
+ * and can serve as a starting point for developing
+ * applications using uhid.
+ */
+
+/*
+ * UHID Example
+ * This example emulates a basic 3 buttons mouse with wheel over UHID. Run this
+ * program as root and then use the following keys to control the mouse:
+ *   q: Quit the application
+ *   1: Toggle left button (down, up, ...)
+ *   2: Toggle right button
+ *   3: Toggle middle button
+ *   a: Move mouse left
+ *   d: Move mouse right
+ *   w: Move mouse up
+ *   s: Move mouse down
+ *   r: Move wheel up
+ *   f: Move wheel down
+ *
+ * Additionally to 3 button mouse, 3 keyboard LEDs are also supported (LED_NUML,
+ * LED_CAPSL and LED_SCROLLL). The device doesn't generate any related keyboard
+ * events, though. You need to manually write the EV_LED/LED_XY/1 activation
+ * input event to the evdev device to see it being sent to this device.
+ *
+ * If uhid is not available as /dev/uhid, then you can pass a different path as
+ * first argument.
+ * If <linux/uhid.h> is not installed in /usr, then compile this with:
+ *   gcc -o ./uhid_test -Wall -I./include ./samples/uhid/uhid-example.c
+ * And ignore the warning about kernel headers. However, it is recommended to
+ * use the installed uhid.h if available.
+ */
+
+/*
+ * HID Report Desciptor
+ * We emulate a basic 3 button mouse with wheel and 3 keyboard LEDs. This is
+ * the report-descriptor as the kernel will parse it:
+ *
+ * INPUT(1)[INPUT]
+ *   Field(0)
+ *     Physical(GenericDesktop.Pointer)
+ *     Application(GenericDesktop.Mouse)
+ *     Usage(3)
+ *       Button.0001
+ *       Button.0002
+ *       Button.0003
+ *     Logical Minimum(0)
+ *     Logical Maximum(1)
+ *     Report Size(1)
+ *     Report Count(3)
+ *     Report Offset(0)
+ *     Flags( Variable Absolute )
+ *   Field(1)
+ *     Physical(GenericDesktop.Pointer)
+ *     Application(GenericDesktop.Mouse)
+ *     Usage(3)
+ *       GenericDesktop.X
+ *       GenericDesktop.Y
+ *       GenericDesktop.Wheel
+ *     Logical Minimum(-128)
+ *     Logical Maximum(127)
+ *     Report Size(8)
+ *     Report Count(3)
+ *     Report Offset(8)
+ *     Flags( Variable Relative )
+ * OUTPUT(2)[OUTPUT]
+ *   Field(0)
+ *     Application(GenericDesktop.Keyboard)
+ *     Usage(3)
+ *       LED.NumLock
+ *       LED.CapsLock
+ *       LED.ScrollLock
+ *     Logical Minimum(0)
+ *     Logical Maximum(1)
+ *     Report Size(1)
+ *     Report Count(3)
+ *     Report Offset(0)
+ *     Flags( Variable Absolute )
+ *
+ * This is the mapping that we expect:
+ *   Button.0001 ---> Key.LeftBtn
+ *   Button.0002 ---> Key.RightBtn
+ *   Button.0003 ---> Key.MiddleBtn
+ *   GenericDesktop.X ---> Relative.X
+ *   GenericDesktop.Y ---> Relative.Y
+ *   GenericDesktop.Wheel ---> Relative.Wheel
+ *   LED.NumLock ---> LED.NumLock
+ *   LED.CapsLock ---> LED.CapsLock
+ *   LED.ScrollLock ---> LED.ScrollLock
+ *
+ * This information can be verified by reading /sys/kernel/debug/hid/<dev>/rdesc
+ * This file should print the same information as showed above.
+ */
+
+const RDESC: [u8; 85] = [
+    0x05, 0x01,	/* USAGE_PAGE (Generic Desktop) */
+    0x09, 0x02,	/* USAGE (Mouse) */
+    0xa1, 0x01,	/* COLLECTION (Application) */
+    0x09, 0x01,		/* USAGE (Pointer) */
+    0xa1, 0x00,		/* COLLECTION (Physical) */
+    0x85, 0x01,			/* REPORT_ID (1) */
+    0x05, 0x09,			/* USAGE_PAGE (Button) */
+    0x19, 0x01,			/* USAGE_MINIMUM (Button 1) */
+    0x29, 0x03,			/* USAGE_MAXIMUM (Button 3) */
+    0x15, 0x00,			/* LOGICAL_MINIMUM (0) */
+    0x25, 0x01,			/* LOGICAL_MAXIMUM (1) */
+    0x95, 0x03,			/* REPORT_COUNT (3) */
+    0x75, 0x01,			/* REPORT_SIZE (1) */
+    0x81, 0x02,			/* INPUT (Data,Var,Abs) */
+    0x95, 0x01,			/* REPORT_COUNT (1) */
+    0x75, 0x05,			/* REPORT_SIZE (5) */
+    0x81, 0x01,			/* INPUT (Cnst,Var,Abs) */
+    0x05, 0x01,			/* USAGE_PAGE (Generic Desktop) */
+    0x09, 0x30,			/* USAGE (X) */
+    0x09, 0x31,			/* USAGE (Y) */
+    0x09, 0x38,			/* USAGE (WHEEL) */
+    0x15, 0x81,			/* LOGICAL_MINIMUM (-127) */
+    0x25, 0x7f,			/* LOGICAL_MAXIMUM (127) */
+    0x75, 0x08,			/* REPORT_SIZE (8) */
+    0x95, 0x03,			/* REPORT_COUNT (3) */
+    0x81, 0x06,			/* INPUT (Data,Var,Rel) */
+    0xc0,			/* END_COLLECTION */
+    0xc0,		/* END_COLLECTION */
+    0x05, 0x01,	/* USAGE_PAGE (Generic Desktop) */
+    0x09, 0x06,	/* USAGE (Keyboard) */
+    0xa1, 0x01,	/* COLLECTION (Application) */
+    0x85, 0x02,		/* REPORT_ID (2) */
+    0x05, 0x08,		/* USAGE_PAGE (Led) */
+    0x19, 0x01,		/* USAGE_MINIMUM (1) */
+    0x29, 0x03,		/* USAGE_MAXIMUM (3) */
+    0x15, 0x00,		/* LOGICAL_MINIMUM (0) */
+    0x25, 0x01,		/* LOGICAL_MAXIMUM (1) */
+    0x95, 0x03,		/* REPORT_COUNT (3) */
+    0x75, 0x01,		/* REPORT_SIZE (1) */
+    0x91, 0x02,		/* Output (Data,Var,Abs) */
+    0x95, 0x01,		/* REPORT_COUNT (1) */
+    0x75, 0x05,		/* REPORT_SIZE (5) */
+    0x91, 0x01,		/* Output (Cnst,Var,Abs) */
+    0xc0,		/* END_COLLECTION */
+];
+
 fn main() {
     println!("Hello, world!");
 }
