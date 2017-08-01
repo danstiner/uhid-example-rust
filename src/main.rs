65725f6f681e3f3a12cd11ec16e508951421afb0
Daniel Stiner <danstiner@gmail.com>
2017-07-31 22:59:26 -0700

    Translate main()

--- src/main.rs
+++ src/main.rs
@@ -39,9 +39,17 @@
  */
 
 extern crate libc;
+extern crate mio;
+extern crate nix;
 extern crate termios;
 
+use mio::{Events, Poll, PollOpt, Ready, Token};
+use mio::unix::EventedFd;
+use nix::fcntl;
+use nix::unistd;
 use std::env;
+use std::path::PathBuf;
+use std::process;
 use termios::*;
 
 /*
@@ -173,11 +181,52 @@ fn main() {
                 eprintln!("Usage: {} [{}]", env::args().nth(0).unwrap(), DEFAULT_PATH);
                 return;
             } else {
-                arg.clone()
+                PathBuf::from(arg)
             }
         }
-        None => DEFAULT_PATH.to_string()
+        None => PathBuf::from(DEFAULT_PATH)
     };
 
-    println!("Hello, world!");
+    eprintln!("Open uhid-cdev {}", path.to_str().unwrap());
+    let fd = fcntl::open(&path, fcntl::O_RDWR | fcntl::O_CLOEXEC, nix::sys::stat::S_IRUSR | nix::sys::stat::S_IWUSR | nix::sys::stat::S_IRGRP | nix::sys::stat::S_IWGRP).map_err(|err| format!("Cannot open uhid-cdev {}: {}", path.to_str().unwrap(), err)).unwrap();
+
+    eprintln!("Create uhid device");
+    // ret = create(fd);
+    // if (ret) {
+    //     unistd::close(fd);
+    //     process::exit(libc::EXIT_FAILURE);
+    // }
+
+    const STDIN: Token = Token(0);
+    const UHID_DEVICE: Token = Token(1);
+
+    let poll = Poll::new().unwrap();
+
+    poll.register(&EventedFd(&libc::STDIN_FILENO), STDIN,
+                  Ready::readable(), PollOpt::edge()).unwrap();
+    poll.register(&EventedFd(&fd), UHID_DEVICE, Ready::readable(),
+                  PollOpt::edge()).unwrap();
+
+    let mut events = Events::with_capacity(1);
+
+    println!("Press 'q' to quit...");
+    loop {
+        poll.poll(&mut events, None).map_err(|err| eprintln!("Cannot poll for fds: {}", err)).unwrap();
+
+        for event in events.iter() {
+            match event.token() {
+                STDIN => {
+                    println!("keypress");
+                    // keyboard()
+                }
+                UHID_DEVICE => {
+                    // event()
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    println!("Destroy uhid device");
+    // destroy()
 }
